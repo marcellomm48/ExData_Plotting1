462592d4dfb3e5752319aa50d94a779008108aae
marcellomm48 <marcelloridesabike@gmail.com>
2019-12-12 09:43:52 -0800

    Reads, cleans data correctly.
    Could use calirifcation on POSIXct vs as.DATE but it's working now. Are times necessary?
    
    Creates frist chart. Need to figure out line charts.

--- plot1.R
+++ plot1.R
@@ -1,16 +1,34 @@
 #plots plot 1
 
+
+library(dplyr)
+library(lubridate)
 #read file
 
 data <- read.table("C:/Users/molinarm/Desktop/Coursera/Course 4- Exploratory/Week 1/household_power_consumption.txt", 
                    sep = ";", 
-                   header = TRUE) 
+                   header = TRUE,
+                   stringsAsFactors = FALSE
+                   ) 
                    #,nrows = 100 )
 
-time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+#this doesn't work, giveserror(Column `date_time` is of 
+#unsupported class POSIXlt; please use POSIXct instead)
+#but I like the look of this more!
+mutate(data, date_time = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S"))
+febdays <- filter(data, date_time > "2007-02-01" & date_time < "2007-02-03")
 
-data[time > "2007-02-01" & time < "2007-02-03",]
+#gets rid of time data
+data1 <- mutate(data, date_time = as.Date(paste(data$Date, data$Time), "%d/%m/%Y"))
+febdays <- filter(data1, date_time > "2007-02-01" & date_time < "2007-02-03")
 
 
+#this works
+#time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+#data[time > "2007-02-01" & time < "2007-02-03",]
 
-#can i be more efficient with this? Filter first? or mutate/transform the data.frame and then filter? Yes! Filter!!
+febdays$Global_active_power <- as.numeric(febdays$Global_active_power)
+
+hist(febdays$Global_active_power, col = "Red")
+
+with(febdays, plot(date_time, Global_active_power))
