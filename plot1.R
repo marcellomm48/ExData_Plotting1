f0529f86c04b16df089ad8e3f9877b5e3f59b4ce
marcellomm48 <marcelloridesabike@gmail.com>
2019-12-13 13:06:12 -0800

    All plots functioning!
    The final 4 se tplots are not quite sized correctly and the y-axis of #4, 4 is missing .5...
    
    but other thant hat, great!

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
 #plots plot 1
 
-
 library(dplyr)
 library(lubridate)
 #read file
@@ -18,48 +17,45 @@ febdata <- data[as.Date(data$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(data$Da
 date_time <- strptime(paste(febdata$Date, febdata$Time), "%d/%m/%Y %H:%M:%S")
 
 febdata <- cbind(febdata, date_time)
+febdata$date_time <- as.POSIXct(febdata$date_time)
+
+#set par = to box, not what it wants ("m")
+par(pty = "s")
 
 #sucess! plots #1
-hist(febdata$Global_active_power, col = "Red")
+hist(febdata$Global_active_power, 
+     col = "Red", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", 
+     main = "GLobal Active Power")
 
 #sucess #2!
-plot(as.POSIXct(febdata$date_time), febdata$Global_active_power, type = "l")
+plot(febdata$date_time, febdata$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 
+# plot #3
+with(febdata, plot(date_time, Sub_metering_1, col = "black", type = "l", ylab = "Energy sub metering", xlab = ""))
+with(febdata, lines(date_time, Sub_metering_2, col = "red"))
+with(febdata, lines(date_time, Sub_metering_3, col = "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = c(1,1), col = c("black", "red", "blue"))
 
+#4 plot 4!
+par(mfrow = c(2,2), pty = "m")
+plot(febdata$date_time, febdata$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+plot(febdata$date_time, febdata$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
+with(febdata, plot(date_time, Sub_metering_1, col = "black", type = "l", ylab = "Energy sub metering", xlab = ""))
+with(febdata, lines(date_time, Sub_metering_2, col = "red"))
+with(febdata, lines(date_time, Sub_metering_3, col = "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = c(1,1), col = c("black", "red", "blue"), bty = "n")
+
+plot(febdata$date_time, febdata$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 -----------------------------------
 
-#this works
+#this also works
         #whole list of date/times POSIXlt
 date_time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-        
         #list of true/false for feb days
 febdate_time <- date_time >= "2007-02-01" & date_time < "2007-02-03"
-
 febdata <- cbind(data[febdate_time, ], date_time = date_time[febdate_time])
 
-#not needed after strings as factors = TRUE
-#febdata$Global_active_power <- as.numeric(febdata$Global_active_power)
 
-#sucess! plots
-hist(febdata$Global_active_power, col = "Red")
-
-#sucess #2!
-plot(as.POSIXct(febdata$date_time), febdata$Global_active_power, type = "l")
-
-
-#maybe later
-with(febdata, plot(time, Global_active_power, type = "l"))
-with(febdata, geom_line(date_time, Global_active_power))
-
-
-#this doesn't work, giveserror(Column `date_time` is of 
-#unsupported class POSIXlt; please use POSIXct instead)
-#but I like the look of this more!
-mutate(data, date_time = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S"))
-febdays <- filter(data, date_time > "2007-02-01" & date_time < "2007-02-03")
-
-#as.Date() gets rid of time data, as.POSIXct doesn't, but also doesn't work...
-data1 <- mutate(data, date_time = as.Date(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S"))
-data1 <- mutate(data, date_time = as.POSIXct(paste(data$Date, data$Time)))
-febdays <- filter(data1, date_time > "2007-02-01" & date_time < "2007-02-03")
